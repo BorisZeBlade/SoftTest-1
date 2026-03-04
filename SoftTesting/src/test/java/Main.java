18fb6d169da41b0b40536040361e9596b1436407
BorisZeBlade <romannesterenko@arizona.edu>
2026-03-04 13:00:58 +0500

    Delete SoftTesting directory

--- SoftTesting/src/test/java/Main.java
+++
@@ -1,8 +0,0 @@
-import execise1.Product;
-
-public class Main {
-    public static void main(String[] args) {
-        Product product = new Product(1, "Laptop", 1000.0, 10);
-        System.out.println(product);
-    }
-}
